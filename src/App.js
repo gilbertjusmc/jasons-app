de35939acd92d1a7cb4afc063c6f12a9a0cbf858
gilbertjusmc <jgilbert1341@gmail.com>
2020-01-07 20:01:27 -0500

    Started using useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar.js';
 import Counter from './Counter.js';
+// import ImageGen from './ImageGen';
 import './App.css';
 
 function App() {
   return (
     <>
-      <NavBar name='Jason' />
-      <Counter name='test' />
+      <NavBar name='NavBar' />
+      <Counter name='Counter' />
+      {/* <ImageGen name='ImageGen' /> */}
     </>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -3,14 +3,14 @@ import React, { useState } from 'react';
 function Counter() {
 	const [count, setCount] = useState(0);
 
-
 	return (
 		<div>
-			<h2>Button was clicked {count} times</h2>
+			<h2>API has been called {count} times</h2>
 			<button onClick={() => setCount(count + 1)}>
 				Click Here
 			</button>
 		</div>
+
 	);
 }
 
